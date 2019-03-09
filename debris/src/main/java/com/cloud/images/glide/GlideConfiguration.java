dd4e0e981799f9a0d7960b23051e75189adbd3d6
李敬欢 <ljh0576123@163.com>
2019-03-09 23:26:00 +0800

    增加图片加载功能(网络图片、文件图片、资源图片、uri图片)

--- debris/src/main/java/com/cloud/images/glide/GlideConfiguration.java
+++ debris/src/main/java/com/cloud/images/glide/GlideConfiguration.java
@@ -7,10 +7,13 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.load.engine.bitmap_recycle.LruBitmapPool;
-import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
+import com.bumptech.glide.load.engine.cache.DiskLruCacheFactory;
 import com.bumptech.glide.load.engine.cache.LruResourceCache;
 import com.bumptech.glide.load.engine.cache.MemorySizeCalculator;
 import com.bumptech.glide.module.GlideModule;
+import com.cloud.images.RxImage;
+
+import java.io.File;
 
 /**
  * Author lijinghuan
@@ -31,10 +34,14 @@ public class GlideConfiguration implements GlideModule {
         int defaultBitmapPoolSize = build.getBitmapPoolSize();
         //图片内存缓存大小为当前应用的总内存的1/4
         builder.setMemoryCache(new LruResourceCache(defaultMemoryCacheSize / 4));
-        builder.setBitmapPool(new LruBitmapPool(defaultBitmapPoolSize));
+        builder.setBitmapPool(new LruBitmapPool(defaultBitmapPoolSize / 4));
         //磁盘缓存(单位字节)
         int cacheSizeBytes = 1024 * 1024 * 256 * 2;
-        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, cacheSizeBytes));
+        //磁盘缓存
+        File cacheDir = RxImage.getInstance().getBuilder().getImageCacheDir();
+        String diskCacheDir = cacheDir.getAbsolutePath();
+        builder.setDiskCache(new DiskLruCacheFactory(diskCacheDir, cacheSizeBytes));
+//        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, cacheSizeBytes));
         //设置图片解码格式
 //        builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
     }
