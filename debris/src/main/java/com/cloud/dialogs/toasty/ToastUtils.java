67e5e8b1675c46710e4007c7499f132d0fa8c051
李敬欢 <ljh0576123@163.com>
2019-05-07 15:56:22 +0800

    Merge branch 'feature_bug'
    * feature_bug:
      ThreadToolUtils内存溢出判断
    
    # Conflicts:
    #	debris/src/main/java/com/cloud/objects/utils/FinalizeUtils.java
    #	gradle.properties

--- debris/src/main/java/com/cloud/dialogs/toasty/ToastUtils.java
+++ debris/src/main/java/com/cloud/dialogs/toasty/ToastUtils.java
@@ -2,15 +2,67 @@ package com.cloud.dialogs.toasty;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorInt;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.StringRes;
 import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
 import com.cloud.debris.R;
+import com.cloud.objects.ObjectJudge;
+import com.cloud.objects.events.RunnableParamsN;
+import com.cloud.objects.handler.HandlerManager;
 import com.cloud.objects.utils.ResUtils;
 
 public class ToastUtils extends BaseToast {
+
+    private static void showToast(Context context,
+                                  CharSequence message,
+                                  Drawable icon,
+                                  @ColorInt int tintColor,
+                                  @ColorInt int textColor,
+                                  int duration,
+                                  boolean withIcon,
+                                  boolean shouldTint) {
+        if (ObjectJudge.isMainThread()) {
+            Toast toast = custom(context, message, icon, tintColor, textColor, duration, withIcon, shouldTint);
+            toast.show();
+        } else {
+            HandlerManager manager = HandlerManager.getInstance();
+            manager.post(new ToastRunnable(context, message, icon, tintColor, textColor, duration, withIcon, shouldTint));
+        }
+    }
+
+    private static class ToastRunnable extends RunnableParamsN {
+
+        private Context context;
+        private CharSequence message;
+        private Drawable icon;
+        private @ColorInt
+        int tintColor;
+        private @ColorInt
+        int textColor;
+        private int duration;
+        private boolean withIcon;
+        private boolean shouldTint;
+
+        public ToastRunnable(Context context, CharSequence message, Drawable icon, @ColorInt int tintColor, @ColorInt int textColor, int duration, boolean withIcon, boolean shouldTint) {
+            this.context = context;
+            this.message = message;
+            this.icon = icon;
+            this.tintColor = tintColor;
+            this.textColor = textColor;
+            this.duration = duration;
+            this.withIcon = withIcon;
+            this.shouldTint = shouldTint;
+        }
+
+        @Override
+        public void run(Object[] objects) {
+            Toast toast = custom(context, message, icon, tintColor, textColor, duration, withIcon, shouldTint);
+            toast.show();
+        }
+    }
 
     /**
      * Toast消息
@@ -19,7 +71,7 @@ public class ToastUtils extends BaseToast {
      * @param message 消息
      * @param icon    消息图标
      */
-    public static void show(Context context, CharSequence message, @DrawableRes int icon) {
+    public static synchronized void show(Context context, CharSequence message, @DrawableRes int icon) {
         //背景色
         int color = ContextCompat.getColor(context, R.color.color_353a3e);
         //文本颜色
@@ -28,8 +80,7 @@ public class ToastUtils extends BaseToast {
         if (icon != 0) {
             drawable = ResUtils.getDrawable(context, icon);
         }
-        Toast toast = custom(context, message, drawable, color, textColor, Toast.LENGTH_SHORT, false, true);
-        toast.show();
+        showToast(context, message, drawable, color, textColor, Toast.LENGTH_SHORT, false, true);
     }
 
     /**
@@ -70,15 +121,14 @@ public class ToastUtils extends BaseToast {
      * @param context 上下文
      * @param message 消息
      */
-    public static void showError(Context context, CharSequence message) {
+    public static synchronized void showError(Context context, CharSequence message) {
         //背景色
         int color = ContextCompat.getColor(context, R.color.color_d50000);
         //文本颜色
         int textColor = ContextCompat.getColor(context, R.color.color_ffffff);
         //图标
         Drawable icon = ResUtils.getDrawable(context, R.drawable.cl_toast_error_white_24dp);
-        Toast toast = custom(context, message, icon, color, textColor, Toast.LENGTH_SHORT, true, true);
-        toast.show();
+        showToast(context, message, icon, color, textColor, Toast.LENGTH_SHORT, true, true);
     }
 
     /**
@@ -105,8 +155,7 @@ public class ToastUtils extends BaseToast {
         int textColor = ContextCompat.getColor(context, R.color.color_ffffff);
         //图标
         Drawable icon = ResUtils.getDrawable(context, R.drawable.cl_toast_success_white_24dp);
-        Toast toast = custom(context, message, icon, color, textColor, Toast.LENGTH_SHORT, true, true);
-        toast.show();
+        showToast(context, message, icon, color, textColor, Toast.LENGTH_SHORT, true, true);
     }
 
     /**
@@ -133,8 +182,7 @@ public class ToastUtils extends BaseToast {
         int textColor = ContextCompat.getColor(context, R.color.color_ffffff);
         //图标
         Drawable icon = ResUtils.getDrawable(context, R.drawable.cl_toast_outline_white_24dp);
-        Toast toast = custom(context, message, icon, color, textColor, Toast.LENGTH_SHORT, true, true);
-        toast.show();
+        showToast(context, message, icon, color, textColor, Toast.LENGTH_SHORT, true, true);
     }
 
     /**
